1a7b2d5dd6439a91ad6c005b474eaac6e6709a9d
Mainul Hossain <71011546+mainulhossain123@users.noreply.github.com>
2024-09-24 16:14:56 +0600

    Update CF_Zones_WAF_Extract.py

--- CF_Zones_WAF_Extract.py
+++ CF_Zones_WAF_Extract.py
@@ -7,10 +7,10 @@ import time
 # Create a session to reuse HTTP connections
 session = requests.Session()
 
-def get_zones(api_token, page, per_page):
+def get_zones(api_key, page, per_page):
     url = "https://api.cloudflare.com/client/v4/zones"
     headers = {
-        "Authorization": f"Bearer {api_token}",
+        "Authorization": f"Bearer {api_key}",
     }
     params = {
         "page": page,
@@ -29,10 +29,10 @@ def get_zones(api_token, page, per_page):
             time.sleep(5)  # Wait for 5 seconds before retrying
     return False, None
 
-def get_firewall_custom_rules(zone_id, api_token):
+def get_firewall_custom_rules(zone_id, api_key):
     url = f"https://api.cloudflare.com/client/v4/zones/{zone_id}/rulesets/phases/http_request_firewall_custom/entrypoint"
     headers = {
-        "Authorization": f"Bearer {api_token}",
+        "Authorization": f"Bearer {api_key}",
     }
     
     retries = 3  # Number of retries in case of failure
@@ -45,14 +45,14 @@ def get_firewall_custom_rules(zone_id, api_token):
     
     return None
 
-def write_to_csv(zone_data, api_token):
+def write_to_csv(zone_data, api_key):
     current_date = datetime.now().strftime("%Y-%m-%d")
     filename = f'/app/firewall_custom_rules_{current_date}.csv'  # Save file in /app directory in Docker
     with open(filename, mode='a', newline='') as file:
         writer = csv.writer(file)
         zone_name = zone_data['name']
         zone_id = zone_data['id']
-        zone_rules = get_firewall_custom_rules(zone_id, api_token)
+        zone_rules = get_firewall_custom_rules(zone_id, api_key)
         if zone_rules and 'result' in zone_rules:
             result = zone_rules['result']
             filtered_rules = [rule for rule in result['rules'] if rule['action'] == 'skip']
@@ -70,17 +70,17 @@ def write_to_csv(zone_data, api_token):
 
 # Example usage
 if __name__ == "__main__":
-    api_token = os.getenv('API_TOKEN')  # Get API token from environment variable
+    api_key = os.getenv('API_KEY')  # Get API token from environment variable
 
     page = 1
     per_page = 1000
     retry_delay = 1  # Initial delay time
     while True:
-        success, zones = get_zones(api_token, page, per_page)
+        success, zones = get_zones(api_key, page, per_page)
         if success:
             if zones:
                 for zone in zones:
-                    write_to_csv(zone, api_token)
+                    write_to_csv(zone, api_key)
                 page += 1
                 retry_delay = 1  # Reset the retry delay on successful fetch
                 # Introduce a delay between requests to avoid rate limit issues
